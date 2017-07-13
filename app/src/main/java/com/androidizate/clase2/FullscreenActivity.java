99a3e688ae224a3d06eb89fe17de454d9a875ffd
DeboTheaux <debotheaux@gmail.com>
2017-07-13 20:56:10 -0300

    Primer hijo de mi app

--- app/src/main/java/com/androidizate/clase2/FullscreenActivity.java
+++ app/src/main/java/com/androidizate/clase2/FullscreenActivity.java
@@ -1,7 +1,9 @@
 package com.androidizate.clase2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -14,5 +16,15 @@ public class FullscreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_fullscreen);
+        findViewById(R.id.btn_start).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(FullscreenActivity.this, MainActivity.class));
+
+            }
+        });
+
     }
+
+
 }
