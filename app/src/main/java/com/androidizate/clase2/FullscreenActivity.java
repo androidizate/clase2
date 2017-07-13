615808e6cf688e24303ce5c3066c16fce054bca6
ollerandres <oller.andres@gmail.com>
2017-07-13 12:47:39 -0300

    Initial commit

---
+++ app/src/main/java/com/androidizate/clase2/FullscreenActivity.java
@@ -0,0 +1,18 @@
+package com.androidizate.clase2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class FullscreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_fullscreen);
+    }
+}
